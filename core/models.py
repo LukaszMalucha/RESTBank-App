738821a967f2357d831965a5bd172b54d608f9bd
Lukasz Malucha <lucasmalucha@gmail.com>
2019-02-15 19:51:13 +0000

    user

--- core/models.py
+++ core/models.py
@@ -1,3 +1,44 @@
+import uuid
+import os
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.conf import settings
 
-# Create your models here.
+
+# Manager Class
+class UserManager(BaseUserManager):
+
+    # extra fields in case we want to extend class in a future
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)  # helper function to lowercase email
+        user.set_password(password)  # hash helper function
+        user.save(using=self._db)  # in case of multiple dbs
+
+        return user
+
+    # create superuser helper function
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+# Model Class
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that allows using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Assign user manager to objects attribute
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # customize to email
+
+    ### Add  AUTH_USER_MODEL to settings
