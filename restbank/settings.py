b0938c57679b240f4fb211c9ffb3a9d1306f6761
Lukasz Malucha <lucasmalucha@gmail.com>
2019-05-06 07:36:35 +0100

    update

--- restbank/settings.py
+++ restbank/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'restbank-api.herokuapp.com', 'restbank-api1.herokuapp.com', os.environ.get('C9_HOSTNAME')]
 
