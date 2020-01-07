75550e5053f8474bfd81294ec1b2901701960afa
Lukasz Malucha <lucasmalucha@gmail.com>
2020-01-07 12:09:10 +0000

    test

--- app/restbank/settings.py
+++ app/restbank/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
 
@@ -23,7 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+if os.getenv('BUILD_ON_TRAVIS', None):
+    SECRET_KEY = "TestTravis"
+else:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
