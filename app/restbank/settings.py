4978fd81fc3432ed7b1b5ba129b2c84273a17253
Lukasz Malucha <lucasmalucha@gmail.com>
2020-01-07 07:07:27 +0000

    docker deployment

--- app/restbank/settings.py
+++ app/restbank/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import env
+# import env
 import dj_database_url
 from django.conf.global_settings import DATETIME_INPUT_FORMATS
 
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'restbank-api.herokuapp.com', 'restbank-api1.herokuapp.com', os.environ.get('C9_HOSTNAME')]
 
